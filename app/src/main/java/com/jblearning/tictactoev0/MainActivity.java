c78fd7793fec0f1201fa9382ffd9307a094fd0c2
bfleury <bfleury@outlook.com>
2017-03-30 10:40:19 -0400

    Version 5

--- app/src/main/java/com/jblearning/tictactoev0/MainActivity.java
+++ app/src/main/java/com/jblearning/tictactoev0/MainActivity.java
@@ -1,78 +1,75 @@
 package com.jblearning.tictactoev0;
 
-import android.graphics.Point;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.GridLayout;
-import android.util.Log;
 
-/*
-    https://developer.android.com/reference/android/widget/GridLayout.html
-    https://blog.stylingandroid.com/gridlayout-part-1/
- */
+        import android.content.DialogInterface;
+        import android.graphics.Color;
+        import android.graphics.Point;
+        import android.os.Bundle;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
-    private TicTacToe tttGame;
-    private Button [][] buttons;
+    private TicTacToe game;
+    private ButtonGridAndTextView tttView;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-        // setContentView( R.layout.activity_main );
-        tttGame = new TicTacToe( );
-        buildGuiByCode( ); //
-    }
-
-    public void buildGuiByCode( ) {
-        // Get width of the screen
+        game = new TicTacToe( );
         Point size = new Point( );
-        getWindowManager( ).getDefaultDisplay( ).getSize( size );
+        getWindowManager().getDefaultDisplay( ).getSize( size );
         int w = size.x / TicTacToe.SIDE;
-
-        // Create the layout manager as a GridLayout
-        GridLayout gridLayout = new GridLayout( this );
-        gridLayout.setColumnCount( TicTacToe.SIDE );
-        gridLayout.setRowCount( TicTacToe.SIDE );
-
-        // Create the buttons and add them to gridLayout
-        buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
-        ButtonHandler bh = new ButtonHandler();
-        for( int row = 0; row < TicTacToe.SIDE; row++ ) {
-            for( int col = 0; col < TicTacToe.SIDE; col++ ) {
-                buttons[row][col] = new Button( this );
-                gridLayout.addView( buttons[row][col], w, w );
-            }
-        }
-
-        // Set gridLayout as the View of this Activity
-        setContentView( gridLayout );
-    } // end buildGuiByCode
-
-    public void update(int row, int col) {
-        int play = tttGame.play( row, col );
-        buttons[row][col].setText( "X" );          if( play == 1 )
-            buttons[row][col].setText( "X" );
-        else if( play == 2 )
-            buttons[row][col].setText( "O" );
-        if( tttGame.isGameOver( ) ) // game over, disable buttons
-            enableButtons( false );
+        ButtonHandler bh = new ButtonHandler( );
+        tttView = new ButtonGridAndTextView( this, w, TicTacToe.SIDE, bh );
+        tttView.setStatusText( game.result( ) );
+        setContentView( tttView );
     }
 
-    public void enableButtons( boolean enabled ) {
-        for( int row = 0; row < TicTacToe.SIDE; row++ )
-            for( int col = 0; col < TicTacToe.SIDE; col++ )
-                buttons[row][col].setEnabled( enabled );
-
+    public void showNewGameDialog( ) {
+        AlertDialog.Builder alert = new AlertDialog.Builder( this );
+        alert.setTitle( "This is fun" );
+        alert.setMessage( "Play again?" );
+        PlayDialog playAgain = new PlayDialog( );
+        alert.setPositiveButton( "YES", playAgain );
+        alert.setNegativeButton( "NO", playAgain );
+        alert.show( );
     }
 
     private class ButtonHandler implements View.OnClickListener {
         public void onClick( View v ) {
-            Log.w( "MainActivity", "Inside onClick, v = " + v );
-            for( int row = 0; row < TicTacToe.SIDE; row++ )
-                for( int column = 0; column < TicTacToe.SIDE; column++ )
-                    if ( v == buttons[row][column] )
-                        update(row, column);
+            for( int row = 0; row < TicTacToe.SIDE; row++ ) {
+                for( int column = 0; column < TicTacToe.SIDE; column++ ) {
+                    if( tttView.isButton( ( Button ) v, row, column ) ) {
+                        int play = game.play( row, column );
+                        if( play == 1 )
+                            tttView.setButtonText( row, column, "X" );
+                        else if( play == 2 )
+                            tttView.setButtonText( row, column, "O" );
+                        if( game.isGameOver( ) ) {
+                            tttView.setStatusBackgroundColor( Color.RED );
+                            tttView.enableButtons( false );
+                            tttView.setStatusText( game.result( ) );
+                            showNewGameDialog( );    // offer to play again
+                        }
+                    }
+                }
+            }
         }
-    } // end ButtonHandler
-} // end main class
+    }
+
+    private class PlayDialog implements DialogInterface.OnClickListener {
+        public void onClick( DialogInterface dialog, int id ) {
+            if( id == -1 ) /* YES button */ {
+                game.resetGame( );
+                tttView.enableButtons( true );
+                tttView.resetButtons( );
+                tttView.setStatusBackgroundColor( Color.GREEN );
+                tttView.setStatusText( game.result( ) );
+            }
+            else if( id == -2 ) // NO button
+                MainActivity.this.finish( );
+        }
+    }
+}
